e09ff231797810064071611541d0ea232a1025ad
Chandan Gowda B S <g-chandan@inahosolutions.com>
2024-09-02 15:15:54 +0530

    Commiting solution for step 4

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -12,11 +12,18 @@ test("if input is string contains only one number, function should return that n
 test("if input is string contains only 2 numbers with ',' as delimiter, function should return sum of 2 numbers", () => {
   expect(stringCalculator.add("1,4")).toBe(5);
 });
+
 //step 2
 test("if input is string contains only n numbers with ',' as delimiter, function should return sum of n numbers", () => {
   expect(stringCalculator.add("1,4,8")).toBe(13);
 });
+
 //step 3
 test("if input is string contains only n numbers with ',' and '\n' as delimiter, function should return sum of n numbers", () => {
   expect(stringCalculator.add("1\n2,3")).toBe(6);
+});
+
+//step 4
+test("Support different delimiters", () => {
+  expect(stringCalculator.add("//;\n1;4")).toBe(5);
 });
